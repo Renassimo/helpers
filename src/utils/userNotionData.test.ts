5bdb369d96b1de4fc038e5feddc1cd0f294c6852
Renas Sitdikov <renas.sitdikov@flatstack.com>
2023-02-08 18:15:25 +0100

    add utils/userNotionData.test.ts

---
+++ src/utils/userNotionData.test.ts
@@ -0,0 +1,66 @@
+import getUserNotionData from '@/utils/userNotionData';
+
+let notionData: unknown;
+let id: unknown;
+
+jest.mock('@/lib/firebase/firestore', () => ({
+  collection: () => ({
+    doc: () => ({
+      get: () => ({
+        data: () => ({
+          notionData,
+        }),
+        id,
+      }),
+    }),
+  }),
+}));
+
+describe('getUserNotionData', () => {
+  const mockedUid = 'uid';
+  const mockedNotionData = {
+    fiveBook: { dataBaseId: 'data-base-id', token: 'token' },
+  };
+
+  describe('when got user', () => {
+    beforeEach(() => {
+      id = mockedUid;
+    });
+
+    describe('and got notion data', () => {
+      beforeEach(() => {
+        notionData = mockedNotionData;
+      });
+
+      test('returns notion data', async () => {
+        // Arrange
+        const expectedResult = {
+          id: mockedUid,
+          notionData: mockedNotionData,
+        };
+        // Act
+        const result = await getUserNotionData(mockedUid);
+        // Assert
+        expect(result).toEqual(expectedResult);
+      });
+    });
+
+    describe('and did not get notion data', () => {
+      beforeEach(() => {
+        notionData = undefined;
+      });
+
+      test('returns notion data', async () => {
+        // Arrange
+        const expectedResult = {
+          id: mockedUid,
+          notionData: null,
+        };
+        // Act
+        const result = await getUserNotionData(mockedUid);
+        // Assert
+        expect(result).toEqual(expectedResult);
+      });
+    });
+  });
+});
--- src/utils/userNotionData.ts
+++ src/utils/userNotionData.ts
@@ -3,9 +3,9 @@ import firestore from '@/lib/firebase/firestore';
 const getUserNotionData = async (uid: string) => {
   try {
     const doc = await firestore.collection('users').doc(uid).get();
-    const { notionData = null } = doc.data() ?? {};
+    const { notionData = null } = doc?.data() ?? {};
 
-    return { id: doc.id, notionData };
+    return { id: doc?.id ?? uid, notionData };
   } catch (error) {
     return { id: uid, notionData: null };
   }
