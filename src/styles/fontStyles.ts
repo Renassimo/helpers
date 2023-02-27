d5cac701c6557996cbd14ab7cf24681d02b05364
Renas Sitdikov <renas.sitdikov@flatstack.com>
2023-02-27 09:54:04 +0100

    update fontStyles

--- src/styles/fontStyles.ts
+++ src/styles/fontStyles.ts
@@ -11,7 +11,9 @@ export const Roboto = css`
     font-family: 'Roboto';
     font-style: normal;
     font-weight: 300;
-    src: url(${RobotoLatin300Normal}) format('woff2');
+    src: url(${RobotoLatin300Normal ??
+      '/fonts/files/roboto-latin-300-normal.woff2'})
+      format('woff2');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
       U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
       U+2215, U+FEFF, U+FFFD;
@@ -21,7 +23,9 @@ export const Roboto = css`
     font-family: 'Roboto';
     font-style: normal;
     font-weight: 400;
-    src: url(${RobotoLatin400Normal}) format('woff2');
+    src: url(${RobotoLatin400Normal ??
+      '/fonts/files/roboto-latin-400-normal.woff2'})
+      format('woff2');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
       U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
       U+2215, U+FEFF, U+FFFD;
@@ -31,7 +35,9 @@ export const Roboto = css`
     font-family: 'Roboto';
     font-style: normal;
     font-weight: 500;
-    src: url(${RobotoLatin500Normal}) format('woff2');
+    src: url(${RobotoLatin500Normal ??
+      '/fonts/files/roboto-latin-500-normal.woff2'})
+      format('woff2');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
       U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
       U+2215, U+FEFF, U+FFFD;
@@ -41,7 +47,9 @@ export const Roboto = css`
     font-family: 'Roboto';
     font-style: normal;
     font-weight: 700;
-    src: url(${RobotoLatin700Normal}) format('woff2');
+    src: url(${RobotoLatin700Normal ??
+      '/fonts/files/roboto-latin-700-normal.woff2'})
+      format('woff2');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
       U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
       U+2215, U+FEFF, U+FFFD;
