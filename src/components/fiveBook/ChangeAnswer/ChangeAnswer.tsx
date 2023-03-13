6ef513115a34469280bf2a22ad122af6fc395ff9
Renas Sitdikov <renas.sitdikov@flatstack.com>
2023-03-13 18:10:39 +0100

    create change answer modal

---
+++ src/components/fiveBook/ChangeAnswer/ChangeAnswer.tsx
@@ -0,0 +1,79 @@
+import { FormEvent, useCallback, useMemo } from 'react';
+import { useRouter } from 'next/router';
+
+import { TextField } from '@mui/material';
+import Typography from '@mui/material/Typography';
+
+import Modal from '@/components/common/Modal';
+
+import useFiveBook from '@/hooks/fiveBook/useFiveBook';
+import useUpdateAnswers from '@/hooks/fiveBook/useUpdateAnswers';
+
+const ChangeAnswer = () => {
+  const { answers, question } = useFiveBook();
+  const { update, loading } = useUpdateAnswers(false);
+  const { query, replace } = useRouter();
+  const { updateYear } = query;
+
+  const year = useMemo(
+    () => (typeof updateYear == 'string' ? updateYear : null),
+    [updateYear]
+  );
+
+  const answer = useMemo(
+    () => answers?.find((answer) => answer.year === year)?.value ?? '',
+    [answers, year]
+  );
+
+  const modalTitle = useMemo(
+    () => (year ? `Update ${year} answer` : ''),
+    [year]
+  );
+
+  const isModalOpen = useMemo(
+    () => !!(year && answers?.find((answer) => answer.year === year)),
+    [answers, year]
+  );
+
+  const handleModalClose = useCallback(async () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { updateYear, ...updatedQuery } = query;
+    await replace({ pathname: '', query: updatedQuery }, undefined, {
+      shallow: true,
+    });
+  }, [query, replace]);
+
+  const onSubmit = useCallback(
+    async (event: FormEvent<HTMLFormElement>) => {
+      // @ts-ignore
+      await update({ [String(year)]: event.target['updateAnswer'].value });
+      await handleModalClose();
+    },
+    [handleModalClose, update, year]
+  );
+
+  return (
+    <Modal
+      open={isModalOpen}
+      onClose={handleModalClose}
+      title={modalTitle}
+      onSubmit={onSubmit}
+      loading={loading}
+    >
+      <Typography component="h3" variant="h6">
+        {question}
+      </Typography>
+      <TextField
+        multiline
+        rows={4}
+        fullWidth
+        margin="dense"
+        defaultValue={answer}
+        autoFocus
+        name="updateAnswer"
+      />
+    </Modal>
+  );
+};
+
+export default ChangeAnswer;
