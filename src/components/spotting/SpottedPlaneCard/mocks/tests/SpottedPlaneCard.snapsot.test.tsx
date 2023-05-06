eb1ff103e3deaefb7c53271d3279262632c0c9dd
Renas Sitdikov <renas.sitdikov@flatstack.com>
2023-05-06 17:17:46 +0200

    add SpottedPlaneCard tests

--- src/components/spotting/SpottedPlaneCard/mocks/tests/SpottedPlaneCard.snapsot.test.tsx
+++ src/components/spotting/SpottedPlaneCard/mocks/tests/SpottedPlaneCard.snapsot.test.tsx
@@ -1,8 +1,13 @@
 import renderWithTheme from '@/tests/helpers';
 
-import SpottedPlaneCard from '@/components/spotting/SpottedPlaneCard';
-import { mockedSpottedPlaneProviderDataTruthy } from '@/types/spotting/mocks/mockedSpottedPlaneProviderData';
+import {
+  mockedSpottedPlaneProviderDataEmpty,
+  mockedSpottedPlaneProviderDataTruthy,
+} from '@/types/spotting/mocks/mockedSpottedPlaneProviderData';
 
+import useSpottedPlanes from '@/hooks/spotting/useSpottedPlanes';
+
+import SpottedPlaneCard from '@/components/spotting/SpottedPlaneCard';
 import SelectableCard from '@/components/spotting/SelectableCard';
 import SpottedPlaneForm from '@/components/spotting/SpottedPlaneForm';
 
@@ -11,16 +16,29 @@ import MockedSpottedPlaneForm from '@/components/spotting/SpottedPlaneForm/mocks
 
 jest.mock('@/components/spotting/SelectableCard');
 jest.mock('@/components/spotting/SpottedPlaneForm');
+jest.mock('@/hooks/spotting/useSpottedPlanes');
 
 describe('SpottedPlaneCard snapshots', () => {
-  test('renders successfully', () => {
-    // Arrange
+  beforeEach(() => {
     (SelectableCard as unknown as jest.Mock).mockImplementation(
       MockedSelectableCard
     );
     (SpottedPlaneForm as unknown as jest.Mock).mockImplementation(
       MockedSpottedPlaneForm
     );
+    (useSpottedPlanes as unknown as jest.Mock).mockImplementation(
+      jest.fn(() => ({
+        selectedIds: [],
+      }))
+    );
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('renders successfully', () => {
+    // Arrange
     // Act
     const { container } = renderWithTheme(
       <SpottedPlaneCard data={mockedSpottedPlaneProviderDataTruthy} />
@@ -33,5 +51,44 @@ describe('SpottedPlaneCard snapshots', () => {
       },
       {}
     );
+  });
+
+  describe('when does not show description', () => {
+    test('renders successfully', () => {
+      // Arrange
+      // Act
+      const { container } = renderWithTheme(
+        <SpottedPlaneCard data={mockedSpottedPlaneProviderDataEmpty} />
+      );
+      // Assert
+      expect(container).toMatchSnapshot();
+      expect(MockedSpottedPlaneForm).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('when photo is selectable', () => {
+    test('renders successfully', () => {
+      // Arrange
+      (useSpottedPlanes as unknown as jest.Mock).mockImplementation(
+        jest.fn(() => ({
+          selectedIds: ['id-1'],
+        }))
+      );
+      // Act
+      const { container } = renderWithTheme(
+        <SpottedPlaneCard
+          selectable
+          data={mockedSpottedPlaneProviderDataTruthy}
+        />
+      );
+      // Assert
+      expect(container).toMatchSnapshot();
+      expect(MockedSpottedPlaneForm).toHaveBeenCalledWith(
+        {
+          data: mockedSpottedPlaneProviderDataTruthy,
+        },
+        {}
+      );
+    });
   });
 });
