7d522056657a5d391d320a8e9f8c4c49f684993a
Renas Sitdikov <renas.sitdikov@flatstack.com>
2023-05-30 14:42:29 +0200

    update textLines utils

--- src/utils/spotting/textLines.ts
+++ src/utils/spotting/textLines.ts
@@ -38,19 +38,24 @@ export const getDescriptionLines = (data: SpottedPlaneProviderData) => {
   const freighterHashtag =
     freighter && cnConverted && insHash(`${freighter}_${cnConverted}`);
 
+  const formattedCn = cn && `(cn ${cn})`;
+  const formattedFirstFlight = acceptedFirstFlight
+    ? `First flight ${formatFromNotionDate(acceptedFirstFlight)}`
+    : null;
+  const formattedSpottedDate = spottedDate
+    ? formatFromNotionDate(spottedDate)
+    : null;
+  const flownHashtag = flown && insHash('renassimo_flown');
+
   return [
     [manufacturer, model, carrier],
     [airplaneName],
-    [registration, cn && `(cn ${cn})`],
-    [
-      acceptedFirstFlight
-        ? `First flight ${formatFromNotionDate(acceptedFirstFlight)}`
-        : null,
-    ],
-    [place, spottedDate ? formatFromNotionDate(spottedDate) : null],
+    [registration, formattedCn],
+    [formattedFirstFlight],
+    [place, formattedSpottedDate],
     [modelCnHashTag],
     [freighterHashtag],
-    [flown && insHash('renassimo_flown')],
+    [flownHashtag],
   ];
 };
 
@@ -69,29 +74,44 @@ export const getHashtagLines = (data: SpottedPlaneProviderData) => {
   const { avrTag: avrCarrierTag = null, commonTag: commonCarrierTag = null } =
     convertedCarrier ? specialHashTags[convertedCarrier] ?? {} : {};
 
+  const convertedModelRTag = insRHash(convertedModel);
+  const convertedManufacturerRTag = insRHash(convertedManufacturer);
+  const convertedCarrierRTag = insRHash(convertedCarrier);
+  const convertedPlaceRTag = insRHash(convertedPlace);
+
+  const isSpottedTag = isSpotted ? insHash('renassimo_spotted') : null;
+  const modelledTag = modelled ? insHash('renassimo_modelled') : null;
+
+  const convertedManufacturerTag = insHash(convertedManufacturer);
+  const convertedModelTag = insHash(convertedModel);
+  const convertedCarrierTag = insHash(convertedCarrier);
+
+  const commonTags =
+    '#spotting #aviation #avgeek #flywithme #planes #jets #jetlovers';
+  const isSpottedCommonTags =
+    isSpotted && '#kznspotting #waw #epwa #wawspotting #warsawspotting';
+  const modelledCommonTags =
+    modelled &&
+    '#aviamodel #aviamodelling #modelkit #scalemodel #plasticmodel #revell #zvezdamodels';
+
   return [
     withEnter([
-      insRHash(convertedModel),
-      insRHash(convertedManufacturer),
+      convertedModelRTag,
+      convertedManufacturerRTag,
       avrModelTag,
       avrCarrierTag,
-      insRHash(convertedCarrier),
-      insRHash(convertedPlace),
+      convertedCarrierRTag,
+      convertedPlaceRTag,
     ]),
-    withEnter([isSpotted ? insHash('renassimo_spotted') : null]),
-    withEnter([modelled ? insHash('renassimo_modelled') : null]),
+    withEnter([isSpottedTag]),
+    withEnter([modelledTag]),
     withEnter([
-      insHash(convertedManufacturer),
-      insHash(convertedModel),
+      convertedManufacturerTag,
+      convertedModelTag,
       commonModelTag,
       commonCarrierTag,
-      insHash(convertedCarrier),
+      convertedCarrierTag,
     ]),
-    [
-      '#spotting #aviation #avgeek #flywithme #planes #jets #jetlovers',
-      isSpotted && '#kznspotting #waw #epwa #wawspotting #warsawspotting',
-      modelled &&
-        '#aviamodel #aviamodelling #modelkit #scalemodel #plasticmodel #revell #zvezdamodels',
-    ],
+    [commonTags, isSpottedCommonTags, modelledCommonTags],
   ];
 };
