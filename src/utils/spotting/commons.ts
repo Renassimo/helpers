e6ba27714d193e8c5a658b419d348de7d6fe223b
Renas Sitdikov <renas.sitdikov@flatstack.com>
2023-05-30 14:42:51 +0200

    add commons util

---
+++ src/utils/spotting/commons.ts
@@ -0,0 +1,44 @@
+import { SpottedPlaneProviderData } from '@/types/spotting';
+
+const isCommon = (
+  key: keyof SpottedPlaneProviderData,
+  data: SpottedPlaneProviderData[]
+) => {
+  const [firstDataItem, ...nextDataItems] = data;
+  return nextDataItems.every(
+    (dataItem) => firstDataItem[key] === dataItem[key]
+  );
+};
+
+export const getCommons = (
+  selectedIds: string[],
+  spottingData: Record<string, SpottedPlaneProviderData>
+) => {
+  const data = selectedIds.map((id) => spottingData[id]);
+
+  const isCommonManufacturer = isCommon('manufacturer', data);
+  const isCommonModel = isCommon('model', data);
+  const isCommonCarrier = isCommon('carrier', data);
+  const isCommonRegistration = isCommon('registration', data);
+  const isCommonCn = isCommon('cn', data);
+  const isCommonPlace = isCommon('place', data);
+  const isCommonSpottedDate = isCommon('spottedDate', data);
+
+  const isCommonPlane =
+    isCommonManufacturer &&
+    isCommonModel &&
+    isCommonCarrier &&
+    isCommonRegistration &&
+    isCommonCn;
+
+  return {
+    isCommonManufacturer,
+    isCommonModel,
+    isCommonCarrier,
+    isCommonRegistration,
+    isCommonCn,
+    isCommonPlace,
+    isCommonSpottedDate,
+    isCommonPlane,
+  };
+};
